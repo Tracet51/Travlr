e2be999eb435fd91bdd6fcc7fb3e57483e44aa5d
Tracet51 <trace@tschida.us>
2017-05-31 17:13:07 -0500

    Fixes to server

--- app.js
+++ app.js
@@ -15,12 +15,16 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-server.post("/api/messages", connector.listen());
 
+// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
 var bot = new builder.UniversalBot(connector);
 function HtmlParse(html) {
     html += " ";
